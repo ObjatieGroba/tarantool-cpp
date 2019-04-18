7268d3965080ec9a40a7293528b841c4d19ee0dd
Igor Mineev <igron99@mail.ru>
2019-04-18 20:24:27 +0300

    Hotfix: tarantool covertly converts float to double

--- tarantool-cpp.hpp
+++ tarantool-cpp.hpp
@@ -754,6 +754,10 @@ public:
             value = mp_decode_float(&data);
             return *this;
         }
+        if (type == MP_DOUBLE) {
+            value = static_cast<float>(mp_decode_double(&data));
+            return *this;
+        }
         throw type_error("Type: " + std::to_string(static_cast<int>(type)) + ", expected MP_FLOAT");
     }
 
@@ -762,6 +766,10 @@ public:
         auto type = mp_typeof(*data);
         if (type == MP_DOUBLE) {
             value = mp_decode_double(&data);
+            return *this;
+        }
+        if (type == MP_FLOAT) {
+            value = static_cast<double>(mp_decode_float(&data));
             return *this;
         }
         throw type_error("Type: " + std::to_string(static_cast<int>(type)) + ", expected MP_DOUBLE");
